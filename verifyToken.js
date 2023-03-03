1f567c7deb72dda51fb9e1eb667afbaff57c40af
CANHDINH31 <dinhphamcanh@gmail.com>
2023-03-03 18:01:07 +0700

    update-search

--- verifyToken.js
+++ verifyToken.js
@@ -4,8 +4,6 @@ import { createError } from "./error.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
-  console.log(token);
-
   if (!token) return next(createError(401, "You are not authenticated !"));
 
   jwt.verify(token, process.env.JWT, (err, user) => {
